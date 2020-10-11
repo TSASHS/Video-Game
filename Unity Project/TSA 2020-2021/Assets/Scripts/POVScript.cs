cbdf120d431076bc6969df7c03896927b816d845
Alterix97 <alterix97@gmail.com>
2020-10-11 18:02:10 -0500

    Looking around, moving, and work on gravity

---
+++ Unity Project/TSA 2020-2021/Assets/Scripts/POVScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class POVScript : MonoBehaviour
+{
+    public float mouseSensitivity = 120f;
+    float y2 = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        y2 -= y;
+        y2 = Mathf.Clamp(y2, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(y2, 0f, 0f);
+        transform.parent.Rotate(Vector3.up * x);
+    }
+}
---
+++ Unity Project/TSA 2020-2021/Assets/Scripts/movementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 1f;
+    public float gravity = 7;
+    public Vector3 fallVelocity;
+
+    public LayerMask ground;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = transform.right * x + transform.forward * y;
+
+        fallVelocity.y -= gravity * Time.deltaTime;
+
+        controller.Move(fallVelocity * Time.deltaTime);
+
+        controller.Move(movementVector * speed *  Time.deltaTime);
+    }
+}
