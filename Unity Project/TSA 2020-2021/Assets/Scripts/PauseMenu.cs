7417425ca1d2dbadec7777671b3ed2971d49db93
Alterix97 <alterix97@gmail.com>
2020-10-17 17:42:29 -0500

    Pause Menu
    Adam and I worked on the pause menu

---
+++ Unity Project/TSA 2020-2021/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu, cursor, tutorialsprite;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            if(pauseMenu.activeSelf == true){
+                Time.timeScale = 0f;
+                Cursor.lockState = CursorLockMode.None;
+                cursor.SetActive(false);
+                tutorialsprite.SetActive(false);
+            }else{
+                unpause();
+                Cursor.lockState = CursorLockMode.Locked;
+                cursor.SetActive(true);
+                tutorialsprite.SetActive(true);
+            }
+        }
+    }
+    public void unpause()
+    {
+        Time.timeScale = 1f;
+    }
+}
