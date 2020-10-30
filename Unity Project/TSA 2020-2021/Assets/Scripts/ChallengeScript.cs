5d7873b723e863eb1dd3673eb9e894d1ea1e1fab
Alterix97 <alterix97@gmail.com>
2020-10-30 15:54:55 -0500

    Handheld Torch and Door Anim

---
+++ Unity Project/TSA 2020-2021/Assets/Scripts/ChallengeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeScript : MonoBehaviour
+{
+    public bool challengeCompleted = false;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(a)
+        {
+            SetChallengeComplete();
+        }
+        */
+        if(Input.GetKeyDown(KeyCode.C)){
+            SetChallengeComplete();
+        }
+    }
+    void SetChallengeComplete ()
+    {
+        challengeCompleted = true;
+        animator.SetBool("ChallengeCompleted", true);
+    }
+}
