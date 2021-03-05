38d1d278cbd49c67e7e08d71ba41cf2d4e0a5980
Alterix97 <alterix97@gmail.com>
2021-03-05 11:47:05 -0600

    Textured Sand and Walls, created script, put player in third room

---
+++ Unity Project/TSA 2020-2021/Assets/Level 3/Scripts/LevelMain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Movement()
+    {
+
+    }
+    void Look()
+    {
+        
+    }
+    void Torch()
+    {
+        
+    }
+}
